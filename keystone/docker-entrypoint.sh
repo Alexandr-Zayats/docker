0f13959577b8a5fc3441e9e9c2c2fdaa5008da96
Alexandr Zayats <alexandr@zayats.org>
2018-02-23 16:17:43 +0200

    - config

--- keystone/docker-entrypoint.sh
+++ keystone/docker-entrypoint.sh
@@ -1,32 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ -z "$KEYSTONE_DB_PASSWORD" ]; then
-    echo >&2 'error: missing KEYSTONE_DB_PASSWORD environment variable'
-    exit 1
-fi
+conf_path="/etc/keystone/keystone.conf"
 
-MYSQL_SERVICE_HOST=${MYSQL_SERVICE_HOST:-$SERVICE_HOST}
-
-if [ -z "$MYSQL_SERVICE_HOST" ]; then
-    echo >&2 'error: Keystone requires access to a MySQL database'
-    exit 1
-fi
-
-KEYSTONE_DB_USER=${KEYSTONE_DB_USER:-keystone}
-KEYSTONE_DB_NAME=${KEYSTONE_DB_NAME:-keystone}
-
-if [[ -z "$(mysql -uroot -p"${MYSQL_ROOT_PASSWD}" -h ${MYSQL_SERVICE_HOST} -s -qfsBe  "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='${KEYSTONE_DB_NAME}'" 2>&1)" ]];
+if [ $KEYSTONE_DATABASE_CONNECTION ]
 then
-    mysql -uroot -p"${MYSQL_ROOT_PASSWD}" -h ${MYSQL_SERVICE_HOST} -e "CREATE DATABASE ${KEYSTONE_DB_NAME} CHARACTER SET UTF8; \
-    GRANT ALL PRIVILEGES ON ${KEYSTONE_DB_NAME}.* TO '${KEYSTONE_DB_USER}'@'%' IDENTIFIED BY '${KEYSTONE_DB_PASSWORD/g}';"
+    crudini --set $conf_path database connection $KEYSTONE_DATABASE_CONNECTION
 fi
 
-sed -e "s/%DB_USER%/$KEYSTONE_DB_USER/g" \
-    -e "s/%DB_PASSWORD%/$KEYSTONE_DB_PASSWORD/g" \
-    -e "s/%DB_NAME%/$KEYSTONE_DB_NAME/g" \
-    -e "s/%DB_HOST%/$MYSQL_SERVICE_HOST/g" \
-    -i /etc/keystone/keystone.conf
+if [ $KEYSTONE_CACHE_MEMCACHE_SERVERS ]
+then
+    crudini --set $conf_path database memcache_servers $KEYSTONE_CACHE_MEMCACHE_SERVERS
+fi
 
 keystone-manage db_sync
+keystone-manage fernet_setup --keystone-user root --keystone-group root
+keystone-manage bootstrap
 
 exec "$@"
