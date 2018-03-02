09512a9a998f34a323fde7d79f6787e5d67ea8f2
Alexandr Zayats <alexandr@zayats.org>
2018-03-02 18:24:01 +0200

    - update controller image

--- openstack/swift/controller/entrypoint.sh
+++ openstack/swift/controller/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#while [ ! -f /etc/swift/initial/object.ring.gz ]
-#do
-#    echo "whaiting for ring files"
-#    sleep 5
-#done
+while [ ! -f /etc/swift/initial/object.ring.gz ]
+do
+    echo "whaiting for ring files"
+    sleep 5
+done
 
-#cp -f /etc/swift/initial/*.ring.gz /etc/swift/
+cp -f /etc/swift/initial/*.ring.gz /etc/swift/
 /usr/bin/swift-proxy-server /etc/swift/proxy-server.conf
