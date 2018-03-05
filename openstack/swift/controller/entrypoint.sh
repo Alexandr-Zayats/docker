a95746669dcbb091e1e4a6b136d8438193dbbb7b
Alexandr Zayats <alexandr@zayats.org>
2018-03-05 17:19:49 +0200

    - configuration

--- openstack/swift/controller/entrypoint.sh
+++ openstack/swift/controller/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+IP=$(ifconfig | awk '/inet 172.16/{print $2}')
+sed -i "s/MANAGEMENT_INTERFACE_IP_ADDRESS/${IP}/g" /etc/swift/proxy-server.conf
+
 while [ ! -f /etc/swift/initial/object.ring.gz ]
 do
     echo "whaiting for ring files"
