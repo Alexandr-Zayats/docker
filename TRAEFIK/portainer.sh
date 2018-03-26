bed2b1356845853f49897fa4221bbe5872b9a41e
Alexandr Zayats <alzay@ciklum.com>
2018-03-26 14:12:20 +0300

    - rabitMQ

--- TRAEFIK/portainer.sh
+++ TRAEFIK/portainer.sh
@@ -1,1 +1,20 @@
 docker run -d -p 9000:9000 --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer
+
+# SWARM 
+docker service create \
+    --name portainer \
+    --publish 9000:9000 \
+    --constraint 'node.role == manager' \
+    --mount type=bind,src=//var/run/docker.sock,dst=/var/run/docker.sock \
+    portainer/portainer \
+    -H unix:///var/run/docker.sock
+
+docker service create \
+    --name portainer \
+    --publish 9000:9000 \
+    --constraint 'node.role == manager' \
+    --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+    --mount type=bind,src=/data,dst=/data \
+    portainer/portainer \
+    -H unix:///var/run/docker.sock
+
