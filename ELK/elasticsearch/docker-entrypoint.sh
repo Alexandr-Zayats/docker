6a1b87016716f5a06103638818abd21bcb33c48b
Alexandr Zayats <alzay@ciklum.com>
2018-04-05 10:28:29 +0300

    - ELK cluster configuration

--- ELK/elasticsearch/docker-entrypoint.sh
+++ ELK/elasticsearch/docker-entrypoint.sh
@@ -1,112 +1,68 @@
 #!/bin/bash
 
-set -e
+set -e                                                                                    
 
-es_opts=''
-
-while IFS='=' read -r envvar_key envvar_value
-do
-    # Elasticsearch env vars need to have at least two dot separated lowercase words, e.g. `cluster.name`
-    if [[ "$envvar_key" =~ ^[a-z]+\.[a-z]+ ]]
-    then
-        if [[ ! -z $envvar_value ]]; then
-          es_opt="-E${envvar_key}=${envvar_value}"
-          es_opts+=" ${es_opt}"
-        fi
-    fi
-done < <(env)
-
-set -- elasticsearch "$@"
+#set -- elasticsearch "$@" ${ES_JAVA_OPTS}
 
 # Add elasticsearch as command if needed
-if [ "${1:0:1}" = '-' ]; then
-	set -- elasticsearch "$@" ${es_opt}
-fi
+#if [ "${1:0:1}" = '-' ]; then
+#  set -- elasticsearch "$@" ${ES_JAVA_OPTS} 
+#fi
 
 export BASE='/usr/share/elasticsearch'
 CONFIG=${BASE}/config/elasticsearch.yml
 
+sed -ri "s!^(\#\s*)?(cluster\.name:).*!\2 ${CLUSTER_NAME}!" $CONFIG
+sed -ri "s!^(\#\s*)?(node\.name:).*!\2 ${HOSTNAME}!" $CONFIG
+sed -ri "s!^(\#\s*)?(discovery\.zen\.ping\.unicast\.hosts:).*!\2 [\"elastic-master\", \"elasticsearch\", \"elastic-data\"]!" $CONFIG
+
 # = MASTER NODE =                              #
-if [ "$1" = 'master' -a "$(id -u)" = '0' ]; then
+if [ "${HOSTNAME}" = 'elastic-master' -a "$(id -u)" = '0' ]; then
 	# Change node into a data master
 	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 true!" $CONFIG
-    sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 false!" $CONFIG
-    sed -ri "s!^(\#\s*)?(node\.data:).*!\2 false!" $CONFIG
-
-	echo "node.name: [\"elastic-master\"]" >> $CONFIG
-	for path in \
-	    /usr/share/elasticsearch/data \
-	    /usr/share/elasticsearch/logs \
-	    /usr/share/elasticsearch/config \
-    	/usr/share/elasticsearch/plugins
-	do
-    	chown -R elasticsearch:elasticsearch "$path"
-	done
-	set -- su-exec elasticsearch "$@" ${es_opt}
+  sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 false!" $CONFIG
+  sed -ri "s!^(\#\s*)?(node\.data:).*!\2 false!" $CONFIG
 fi
 
 # = INGEST NODE =                              #
-if [ "$1" = 'ingest' -a "$(id -u)" = '0' ]; then
+if [ "${HOSTNAME}" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 	# Change node into a client node
 	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 false!" $CONFIG
-    sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 true!" $CONFIG
-    sed -ri "s!^(\#\s*)?(node\.data:).*!\2 false!" $CONFIG
-
-	# Set master.node's name
-	if ! grep -q "discovery.zen.ping.unicast.hosts" $CONFIG; then
-		echo "discovery.zen.ping.unicast.hosts: [\"elastic-master\", \"elastic-client\", \"elastic-data\"]" >> $CONFIG
-	fi
-	echo "node.name: [\"elastic-client\"]" >> $CONFIG
-	for path in \
-        /usr/share/elasticsearch/data \
-        /usr/share/elasticsearch/logs \
-        /usr/share/elasticsearch/config \
-        /usr/share/elasticsearch/plugins
-    do
-        chown -R elasticsearch:elasticsearch "$path"
-    done
-    set -- su-exec elasticsearch "$@" ${es_opt}
+  sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 true!" $CONFIG
+  sed -ri "s!^(\#\s*)?(node\.data:).*!\2 false!" $CONFIG
 fi
 
 # = DATA NODE =                                #
-if [ "$1" = 'data' -a "$(id -u)" = '0' ]; then
+if [ "${HOSTNAME}" = 'elastic-data' -a "$(id -u)" = '0' ]; then
 	# Change node into a data node
 	sed -ri "s!^(\#\s*)?(node\.master:).*!\2 false!" $CONFIG
-    sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 false!" $CONFIG
-    sed -ri "s!^(\#\s*)?(node\.data:).*!\2 true!" $CONFIG
-
-	# Set master.node's name
-	if ! grep -q "discovery.zen.ping.unicast.hosts" $CONFIG; then
-		echo "discovery.zen.ping.unicast.hosts: [\"elastic-masrter\", \"elastic-client\", \"elastic-data\"]" >> $CONFIG
-	fi
-	echo "node.name: [\"elastic-data\"]" >> $CONFIG
-	for path in \
-        /usr/share/elasticsearch/data \
-        /usr/share/elasticsearch/logs \
-        /usr/share/elasticsearch/config \
-        /usr/share/elasticsearch/plugins
-    do
-        chown -R elasticsearch:elasticsearch "$path"
-    done
-    set -- su-exec elasticsearch "$@" ${es_opt}
+  sed -ri "s!^(\#\s*)?(node\.ingest:).*!\2 false!" $CONFIG
+  sed -ri "s!^(\#\s*)?(node\.data:).*!\2 true!" $CONFIG
 fi
 
-#rm -rf $BASE/plugins/x-pack/platform/linux-x86_64 
-#echo "xpack.ml.enabled: false" >> $CONFIG
+# Drop root privileges if we are running elasticsearch                                    
+# allow the container to be started with `--user`                                         
+#if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+# Change the ownership of user-mutable directories to elasticsearch               
+for path in \
+  /usr/share/elasticsearch/data \
+  /usr/share/elasticsearch/logs \
+  /usr/share/elasticsearch/config \
+  /usr/share/elasticsearch/plugins \
+  /tmp
+do
+  chown -R elasticsearch:elasticsearch "$path"
+done
 
-if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
-	for path in \
-        /usr/share/elasticsearch/data \
-        /usr/share/elasticsearch/logs \
-        /usr/share/elasticsearch/config \
-        /usr/share/elasticsearch/plugins \
-		/tmp
-    do
-        chown -R elasticsearch:elasticsearch "$path"
-    done
-    set -- su-exec elasticsearch "$@" ${es_opt}
-#    #exec su-exec elasticsearch "$BASH_SOURCE" "$@"
-fi
+#set -- su-exec elasticsearch "$@"
 
-echo "$@"
-exec "$@"
+#exec su-exec elasticsearch "$BASH_SOURCE" "$@"                                   
+#fi
+
+# As argument is not related to elasticsearch,                                            
+# then assume that user wants to run his own process,                                     
+# for example a `bash` shell to explore this image 
+#echo "$@"
+#exec "$@"
+su-exec elasticsearch elasticsearch
+
