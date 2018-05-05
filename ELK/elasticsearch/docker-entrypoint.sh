61a7b33d7f4497623996de057985febb4ebbb2be
Alexandr Zayats <alzay@ciklum.com>
2018-05-05 14:36:44 +0300

    - ci configuration

--- ELK/elasticsearch/docker-entrypoint.sh
+++ ELK/elasticsearch/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 export BASE='/usr/share/elasticsearch'
 CONFIG=${BASE}/config/elasticsearch.yml
 
-sed -ri "s!^(\#\s*)?(cluster\.name:).*!\2 ${CLUSTER_NAME}!" $CONFIG
+#sed -ri "s!^(\#\s*)?(cluster\.name:).*!\2 ${CLUSTER_NAME}!" $CONFIG
 
 for path in \
   /usr/share/elasticsearch/data \
